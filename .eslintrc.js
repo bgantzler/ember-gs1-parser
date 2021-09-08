c88d2949e648637c603aeaefa5e7a57714a239c8
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2021-09-08 14:01:39 -0400

    project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,9 @@ module.exports = {
   env: {
     browser: true,
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': 0, // disable prettier while linting, using lint-staged
+  },
   overrides: [
     // node files
     {
